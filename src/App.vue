9dc47701ec311d17563f4d0f4b76a32f744992ac
Dennis Piero Quevedo Yucra <130594683+lomatosiono1@users.noreply.github.com>
2023-05-05 22:11:08 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,181 @@
+<template>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VITAL-CHECK</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<header>
+    <nav>
+        <a href="https://polite-melomakarona-5f4b93.netlify.app">Inicio</a>
+        <a href="https://google.com">Acerca de</a>
+        <a href="#">Servicios</a>
+        <a href="#">Precios</a>
+        <a href="#">Contáctanos</a>
+    </nav>
+    <section class="textos-header">
+        <h1>VITAL-CHECK</h1>
+        <h2>Está enfocada en las farmacias, Para brindar un sistema web
+        de control y ventas</h2>
+        <a href="https://polite-melomakarona-5f4b93.netlify.app">
+            <button class="button-inicio" role="button">Empieza ahora</button>
+        </a>
+    </section>
+    <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+                                                                    style="height: 100%; width: 100%;">
+        <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+              style="stroke: none; fill: #fff;"></path>
+    </svg></div>
+</header>
+<main>
+    <section class="contenedor sobre-nosotros">
+        <h2 class="titulo">Sobre nosotros</h2>
+        <div class="contenedor-sobre-nosotros">
+            <img src="img/PARANOSO.png" alt="" class="imagen-about-us">
+            <div class="contenido-textos">
+                <h3><span>1</span>Un sistema sofisticado para la gestión de ventas</h3>
+                <p>Te brindamos un sistema sofisticado con una variedad de opciones para la gestión
+                    de ventas que controlara toda tu farmacia.</p>
+                <h3><span>2</span>Un sistema para la venta de tus productos</h3>
+                <p>Te brindamos un sistema con el cual podrás vender tus productos al cliente
+                    el cual generara el ticket de venta y así podrás agilizar tus ventas.</p>
+            </div>
+        </div>
+    </section>
+    <section class="portafolio">
+        <div class="contenedor">
+            <h2 class="titulo">INTERFAZ</h2>
+            <div class="galeria-port">
+                <div class="imagen-port">
+                    <img src="img/img1.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/img2.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/logo_fondo.png" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/img1.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/2intef.png" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/img5.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/img6.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+                <div class="imagen-port">
+                    <img src="img/1interfas.png" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/icono1.png" alt="">
+                        <p>Nuestro trabajo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="clientes contenedor">
+        <h2 class="titulo">NUESTRO SISTEMA ESTA DIRIGIDO PARA</h2>
+        <div class="cards">
+            <div class="card">
+                <img src="img/dueñofarma.avif" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Dueños de farmacia y boticas </h4>
+                    <p>Nuestro sistema va dirigido para toda persona que tenga su farmacia
+                    y desee un sistema para el control de su farmacia.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="img/PROVEEDOR.png" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Proveedor de medicamentos</h4>
+                    <p>Nuestro sistema va dirigido también para proveedores que deseen
+                    proveer medicamentos con un sistema que pueda hacer el trabajo por ti.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about-services">
+        <div class="contenedor">
+            <h2 class="titulo">PRECIOS</h2>
+            <div class="servicio-cont">
+                <div class="servicio-ind">
+                    <img src="img/FREE.png" alt="">
+                    <h3>VERSION FREE</h3>
+                    <p>Prueba gratuita de 15 días para probar el sistema.</p>
+                </div>
+                <div class="servicio-ind">
+                    <img src="img/PLUS.png" alt="">
+                    <h3>VERSION PLUS</h3>
+                    <p>Contarás con todas las opciones del sistema para tu Farmacia.</p>
+                </div>
+                <div class="servicio-ind">
+                    <img src="img/PREMIUM.png" alt="">
+                    <h3>VERSION PREMIUM</h3>
+                    <p>Con la versión premium va dirigido para controlar de 2 a más farmacias.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+    <div class="contenedor-footer">
+        <div class="content-foo">
+            <h4>Correo</h4>
+            <p>vitalcheck@startup.com</p>
+        </div>
+        <div class="content-foo">
+            <h4>Términos y Condiciones</h4>
+            <p>Políticas y privacidad</p>
+        </div>
+        <div class="content-foo">
+            <h4>Location</h4>
+            <p>Lima - Perú</p>
+        </div>
+    </div>
+    <h2 class="titulo-final">&copy; VITALCHECK | 2023</h2>
+</footer>
+</body>
+
+</html>
+</template>
+<script setup>
+</script>
